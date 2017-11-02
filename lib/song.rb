94232f2f1972a011e932a5c34f11f8bae394a1e0
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-02 20:40:33 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << artist unless @@artists.include?[artist]
+    @@genres << genre unless @@genres.include?[genre]
+    
+  end
+
+  def self.count 
+    @@count
+  end
+
+  def self.artists
+    @@artists
+  end
+
+  def self.genres
+    @@genres
+  end
+
+end
+  
+    
